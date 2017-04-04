260f9b6fea53a7e54f3bd901058c3f7f4998c934
Jurek Nadzionek <jurekoo@gmail.com>
2017-04-04 17:01:06 +0200

    Comments edit. :)

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 var br = "<br />";
 
-<!-- JS: Max and Min values
--->
+// JS: Max and Min values
+//
 document.write(Number.MAX_VALUE + br);
 document.write(Number.MIN_VALUE + br);
 
-<!-- JS: Escaped characters
--->
+// JS: Escaped characters
+//
 var samp_str = "Here some escaped character \" \' \\ \t \n";
 document.write(br + samp_str + br);
 
-<!-- JS: Combining strings, lenght of string, substring, charAt, indexOf
--->
+// JS: Combining strings, lenght of string, substring, charAt, indexOf
+//
 var first_str = "First String ";
 var second_str = "Second String";
 var combined = first_str + second_str;
@@ -22,8 +22,8 @@ document.write("Substring: " + combined.substring(13,20) + br);
 document.write("Last character: " + combined.charAt(combined.length-2) + br);
 document.write("Index of T is: " + combined.indexOf('g') + br);
 
-<!-- JS: Difference between str_var and num_var, adding and multiplication, Number
--->
+// JS: Difference between str_var and num_var, adding and multiplication, Number
+//
 var str_var = "13";
 var num_var = 31;
 var total = num_var + str_var;
@@ -40,8 +40,8 @@ document.write(br);
 document.write(num_var + num_var + br);
 document.write(num_var + num_var2 + br);
 
-<!-- Float vars, parseInt, typeof
--->
+// Float vars, parseInt, typeof
+//
 var float_var = 3.14159265359;
 var float_str = float_var.toFixed(5);
 document.write(br + "Shortended PI: " + float_str + br);
@@ -49,7 +49,7 @@ document.write("Integer PI: " + parseInt(float_var) + br);
 document.write("Datatype of float_var: " + typeof(float_var) + br);
 document.write("Datatype of float_str: " + typeof(float_str) + br);
 
-<!-- Boolean vars
--->
+// Boolean vars
+//
 bool_var = Boolean(134);
 document.write(br + "Boolean of 134 is: "+ bool_var + br);
