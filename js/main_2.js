dabf657974499abb0f504765f318038f464baa90
Jurek Nadzionek <jurekoo@gmail.com>
2017-06-28 15:57:40 +0200

    Event test continuation :)

--- js/main_2.js
+++ js/main_2.js
@@ -13,3 +13,32 @@ function alertMe()
   console.log(alertmsg);
   return true;
 }
+
+function changeColor()
+{
+  document.getElementById("h3style").style.color="red";
+  document.getElementById("h3style").firstChild.nodeValue="Excited";
+  return true;
+}
+
+function changeAgain()
+{
+  document.getElementById("h3style").style.color="gray";
+  document.getElementById("h3style").firstChild.nodeValue="Bored";
+  return true;
+}
+
+function changeText()
+{
+  document.getElementById("h3style").style.color="blue";
+  document.getElementById("h3style").firstChild.nodeValue="Don't touch me! :[";
+  return true;
+}
+
+function showPara()
+{
+  document.getElementById("first").style.visibility=(document.formex.firstpara.checked) ? "visible" : "hidden";
+  document.getElementById("second").style.visibility=(document.formex.secondpara.checked) ? "visible" : "hidden";
+  document.getElementById("third").style.visibility=(document.formex.thirdpara.checked) ? "visible" : "hidden";
+  return:true;
+}
